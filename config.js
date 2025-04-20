dac71c0e8a75862891a587bd26f7a232dbf93699
YUJI-9 <smady8677@gmail.com>
2025-04-20 14:37:44 +0300

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "B.m.b-Xmd~cLkxmQpZ#AAnNlV1foaTUlrtAGU2rz70JCoLIKeNGdzEISJGizyQ",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
